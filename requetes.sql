781ebefa5e12fd0650f747f288fe14c7498fbe4d
DioufFLR <geoffreyfleur@gmail.com>
2022-12-22 16:42:08 +0100

    1.3

--- requetes.sql
+++ requetes.sql
@@ -9,6 +9,16 @@ WHERE customers.Country = 'France';
 SELECT products.ProductName AS 'Produit', products.UnitPrice AS 'Prix'
 FROM products
 JOIN suppliers ON suppliers.SupplierID = products.SupplierID
-WHERE suppliers.CompanyName = 'Exotic Liquids'
+WHERE suppliers.CompanyName = 'Exotic Liquids';
 
 -- 3 Nombre de produits mis à disposition par les fournisseurs français
+
+SELECT suppliers.CompanyName AS 'Fourinsseur', COUNT(products.SupplierID) AS 'NbreProduits'
+FROM suppliers
+JOIN products ON products.SupplierID = suppliers.SupplierID
+WHERE suppliers.Country = 'France'
+GROUP BY suppliers.CompanyName
+ORDER BY NbreProduits DESC;
+
+-- 4 Liste des clients français ayant passé plus de 10 commandes
+
