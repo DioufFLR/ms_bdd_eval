3569721290edbe47f14c599e4d4bfb80fc1c7433
DioufFLR <geoffreyfleur@gmail.com>
2023-01-03 09:06:26 +0100

    end

--- requetes.sql
+++ requetes.sql
@@ -31,3 +31,46 @@ HAVING COUNT(orders.OrderID) > 10;
 
 -- 5 Liste des clients dont le montant cumulé de toutes les commandes passées est supérieur à 3000 $
 
+SELECT  CompanyName as "Client", SUM(UnitPrice * Quantity) as "CA", Country as "Pays" 
+FROM `customers` 
+JOIN `orders` ON orders.CustomerID = customers.CustomerID
+JOIN `order details` ON `order details`.orderID = orders.OrderID 
+GROUP BY CompanyName HAVING SUM(UnitPrice * Quantity) > 30000
+order by CA DESC;
+
+-- 6 Liste des pays dans lesquels des produits fournis par "Exotic liquids"
+
+SELECT ShipCountry 
+FROM `orders`
+JOIN `order details` ON `order details`.OrderID = orders.OrderID
+JOIN products ON products.ProductID = `order details`.ProductID
+JOIN suppliers ON suppliers.SupplierID = products.SupplierID
+WHERE suppliers.CompanyName = 'Exotic Liquids'
+GROUP BY ShipCountry;
+
+-- 7 Chiffre d'affaires global sur les ventes de 1997
+
+SELECT SUM(UnitPrice * Quantity) AS "CA" 
+FROM `order details`
+JOIN orders ON orders.OrderID = `order details`.`OrderID`
+WHERE YEAR(OrderDate) = 1997;
+
+-- 8 Chiffre d'affaires détaillé par mois sur les ventes de 1997
+
+SELECT SUM(UnitPrice * Quantity) AS "CA" 
+FROM `order details`
+JOIN orders ON orders.OrderID = `order details`.`OrderID`
+WHERE YEAR(OrderDate) = 1997;
+
+
+-- 9 A quand remonte la dernière commande du client nommé "Du monde entier"?
+
+SELECT MAX(OrderDate) AS "Date de denière commande" 
+FROM `orders`
+JOIN customers ON customers.CustomerID = orders.CustomerID
+WHERE CompanyName = "Du monde entier";
+
+-- 10 Quel est délai moyen de livraison en jours?
+
+SELECT round(avg(datediff(ShippedDate, OrderDate))) AS "Délai moyen de livraison en jours"
+FROM `orders`;
